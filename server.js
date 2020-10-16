f4a4c7e087f4bad9ef58d18117febddc90bb7444
HeapifyMe <59264750+HeapifyMe@users.noreply.github.com>
2020-10-16 00:25:54 -0400

    Rewrote app.js in draw.js. Created functions that calculate drawn paths (line, pencil) and remote classes that create strokes across connected clients through the use of sockets

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // set up express server
 var express = require("express");
 var app = express();
@@ -11,30 +12,39 @@ console.log("server running on port: " + portNum);
 var io = require("socket.io")(server);
 
 app.get('/', (req,res) => {
+    console.log("Test");
     res.send('Welcome to Chalkboard');
 });
 
 io.on('connection', (socket) => {
     console.log("new connection: " + socket.id);
+    socket.send("hello");
+
 
     let name = "Guest" +  Math.floor( Math.random() * 10000);
 
     socket.on('startPos', (data) => {
         socket.broadcast.emit('lock');        // broadcast to all sockets except sender who triggered event
+        console.log("Start position");
         io.emit('startPos', data);            // broadcast to all sockets, including sender who triggered event
     });
 
     socket.on('mousePos', (data) => {
         socket.broadcast.emit('lock');
+        console.log("Start position");
         io.emit('mousePos', data);
     });
 
     socket.on('finishPos', () => {
+        console.log("Start position");
+
         io.emit('finishPos');
     });
 
     socket.on("sentMessage", (message) => {
         socket.broadcast.emit(message);
+        console.log("Start position");
+
     });
 
     // chat handling
