f9ddadf673cca9fda89e8d2c7440b2c5a4069822
BT-VT <b.trono90@gmail.com>
2020-10-16 16:21:09 -0400

    minor changes

--- public/js/paperApp.js
+++ public/js/paperApp.js
@@ -1,15 +1,12 @@
 let paths = {};
-var curPath;
+let curPath;
 
 // Create a new path once, when the script is executed:
 function onMouseDown(event) {
 	curPath = new Path();
 	curPath.strokeColor = 'black';
 	paths['path'+ Object.keys(paths).length] = curPath;
-	for(var key in paths) {
-		path = paths[key];
-		path.strokeColor = "red";
-	}
+	
 	console.log(paths);
 }
 // This function is called whenever the user
