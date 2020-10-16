a424c5446439ccdfb28a1255927253160bcdc050
BT-VT <b.trono90@gmail.com>
2020-10-16 16:10:01 -0400

    made paper.js compatible with ES6

--- public/js/paperApp.js
+++ public/js/paperApp.js
@@ -1,13 +1,20 @@
-console.log("hello world");
-var myPath;
+let paths = {};
+var curPath;
+
 // Create a new path once, when the script is executed:
 function onMouseDown(event) {
-	myPath = new Path();
-	myPath.strokeColor = 'black';
+	curPath = new Path();
+	curPath.strokeColor = 'black';
+	paths['path'+ Object.keys(paths).length] = curPath;
+	for(var key in paths) {
+		path = paths[key];
+		path.strokeColor = "red";
+	}
+	console.log(paths);
 }
 // This function is called whenever the user
 // clicks the mouse in the view:
 function onMouseDrag(event) {
 	// Add a segment to the path at the position of the mouse:
-	myPath.add(event.point);
+	curPath.add(event.point);
 }
--- server.js
+++ server.js
@@ -21,31 +21,24 @@ io.on('connection', (socket) => {
     console.log("new connection: " + socket.id);
     socket.send("hello");
 
-
     let name = "Guest" +  Math.floor( Math.random() * 10000);
 
     socket.on('startPos', (data) => {
         socket.broadcast.emit('lock');        // broadcast to all sockets except sender who triggered event
-        console.log("Start position");
         io.emit('startPos', data);            // broadcast to all sockets, including sender who triggered event
     });
 
     socket.on('mousePos', (data) => {
         socket.broadcast.emit('lock');
-        console.log("Start position");
         io.emit('mousePos', data);
     });
 
     socket.on('finishPos', () => {
-        console.log("Start position");
-
         io.emit('finishPos');
     });
 
     socket.on("sentMessage", (message) => {
         socket.broadcast.emit(message);
-        console.log("Start position");
-
     });
 
     // chat handling
