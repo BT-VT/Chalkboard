99d1253d2b9f8c0a37d04b3b7318ccdaab5bd8ee
BT-VT <b.trono90@gmail.com>
2020-11-18 11:22:29 -0500

    changed hashchange routing listener

--- public/js/setup.js
+++ public/js/setup.js
@@ -1,20 +1,41 @@
+// import {socket, paperSockets} from '/js/paperSockets.js';
+//
+// window.addEventListener("hashchange", function () {
+//     if (location.hash == "#/my-chalkboards") {
+//         chalkboardGrid();
+//     } else {
+//         console.log("running paperSockets from hashchange");
+//         paperSockets();
+//
+//     }
+// });
+//
+// // window.addEventListener("load", function() {
+// //     paperSockets();
+// // });
+//
+// window.onload = () => {
+//     console.log("running paperSockets from window.onload()");
+//     paperSockets();
+// }
+
+
 import {socket, paperSockets} from '/js/paperSockets.js';
 
 window.addEventListener("hashchange", function () {
     if (location.hash == "#/my-chalkboards") {
         chalkboardGrid();
+    } /*else {
+        paperSockets();
+
+    }*/
+});
+
+window.addEventListener("load", function() {
+    if (location.hash == "#/my-chalkboards") {
+        //chalkboardGrid();
     } else {
-        console.log("running paperSockets from hashchange");
         paperSockets();
 
     }
 });
-
-// window.addEventListener("load", function() {
-//     paperSockets();
-// });
-
-window.onload = () => {
-    console.log("running paperSockets from window.onload()");
-    paperSockets();
-}
