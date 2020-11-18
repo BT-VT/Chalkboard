aaa68272a50584591707de657971edbdf351e9b7
BT-VT <b.trono90@gmail.com>
2020-11-18 11:26:50 -0500

    refactored setup.js which has new routing

--- public/js/setup.js
+++ public/js/setup.js
@@ -1,41 +1,14 @@
-// import {socket, paperSockets} from '/js/paperSockets.js';
-//
-// window.addEventListener("hashchange", function () {
-//     if (location.hash == "#/my-chalkboards") {
-//         chalkboardGrid();
-//     } else {
-//         console.log("running paperSockets from hashchange");
-//         paperSockets();
-//
-//     }
-// });
-//
-// // window.addEventListener("load", function() {
-// //     paperSockets();
-// // });
-//
-// window.onload = () => {
-//     console.log("running paperSockets from window.onload()");
-//     paperSockets();
-// }
-
-
 import {socket, paperSockets} from '/js/paperSockets.js';
 
 window.addEventListener("hashchange", function () {
     if (location.hash == "#/my-chalkboards") {
         chalkboardGrid();
-    } /*else {
-        paperSockets();
-
-    }*/
+    }
 });
 
 window.addEventListener("load", function() {
-    if (location.hash == "#/my-chalkboards") {
-        //chalkboardGrid();
-    } else {
+    if (location.hash != "#/my-chalkboards") {
+        console.log("running paperSockets from hashchange");
         paperSockets();
-
     }
 });
