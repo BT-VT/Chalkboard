370abc441157390b8c0d2dbcd202e1f7a2730b69
BT-VT <b.trono90@gmail.com>
2020-12-02 19:25:33 -0500

    wrapped picture-in-picture with onload listener

--- public/js/picturetopicture.js
+++ public/js/picturetopicture.js
@@ -1,23 +1,25 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button1');
 
-// Prompt user to select what they want to share 
-async function selectMediaStream(){
-    try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.srcObject= mediaStream;
-        videoElement.onloadedmetadata = ()=>{
-            videoElement.play();
+window.onload = function() {
+    // Prompt user to select what they want to share
+    async function selectMediaStream(){
+        try {
+            const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+            videoElement.srcObject= mediaStream;
+            videoElement.onloadedmetadata = ()=>{
+                videoElement.play();
+            }
+        }catch(error){
+            // Catch error here
         }
-    }catch(error){
-        // Catch error here
     }
-}
 
-button.addEventListener('click',async()=>{
-    
-    button.disabled=true;
-    await videoElement.requestPictureInPicture();
-    button.disabled = false;
-});
-selectMediaStream();
+    button.addEventListener('click',async()=>{
+
+        button.disabled=true;
+        await videoElement.requestPictureInPicture();
+        button.disabled = false;
+    });
+    selectMediaStream();
+}
